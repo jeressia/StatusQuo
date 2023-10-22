89f4e090c5afe4b1f97da7c2879858521b8e19f3
Jeressia Williamson <jeressiajaytv@gmail.com>
2023-10-22 14:16:37 -0500

    feature/appointments

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import "firebase/auth";
-import { getDocs, collection, query, where } from "firebase/firestore";
+import { getDocs, collection, query, where, addDoc } from "firebase/firestore";
 import { db } from "../../utils/firebase";
 import Layout from "../../layout/Layout";
 
@@ -17,29 +17,42 @@ interface DashboardProps {
 const Dashboard = (props: DashboardProps) => {
   const { user, appointments, userId, setAppointments } = props;
   const [loading, setLoading] = useState(false);
-  console.log(appointments);
-  const appointmentsRef = query(
+  console.log("appointments", appointments);
+
+  const appointmentsCollection = collection(db, "appointments");
+
+  const appointmentsByUser = query(
     collection(db, "appointments"),
     where("user_id", "==", 1)
   );
 
+  const getAppointments = async () => {
+    try {
+      const data = await getDocs(appointmentsByUser);
+      const filterData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setAppointments(filterData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onSubmitAppt = async () => {
+    try {
+      await addDoc(appointmentsCollection, {
+        appointment_description: "Checkup",
+        appointment_end_at: new Date("December 17, 2023 03:30:00"),
+        appointment_start_at: new Date(new Date("December 17, 2023 04:30:00")),
+        user_id: 1,
+      });
+      getAppointments();
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
-    const getAppointments = async () => {
-      //Read data
-      try {
-        const data = await getDocs(appointmentsRef);
-        const filterData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-
-        setAppointments(filterData);
-      } catch (err) {
-        console.log(err);
-      }
-      //Set state
-    };
-
     getAppointments();
   }, []);
 
@@ -60,6 +73,7 @@ const Dashboard = (props: DashboardProps) => {
               </li>
             ))}
           </ul>
+          <button onClick={onSubmitAppt}>Create New Appt</button>
         </div>
       </div>
     );
